4c2ee0a890a1a955666e76e6e43786a2c405d465
Alex Zylman <alex.zylman@gmail.com>
2016-04-18 13:10:44 -0700

    tests for alias update

--- test/elasticsearch.ts
+++ test/elasticsearch.ts
@@ -4,6 +4,10 @@ var nock    = require("nock");
 
 var es = require("../lib/elasticsearch");
 
+const today = moment();
+const yesterday = moment().subtract(1, "days");
+const lastMonth = moment().subtract(1, "month");
+const format = (m) => m.format("YYYY.MM.DD");
 
 describe("elasticsearch", () => {
   describe("get_indices", () => {
@@ -16,19 +20,12 @@ describe("elasticsearch", () => {
         assert.deepEqual(indices, expected);
         fakeES.done();
         done();
-      }).catch((err) => {
-        done(err);
-      });
+      }).catch(done);
     });
   });
 
   describe("clear_old_indices", () => {
     it("should make delete requests for all old indices", (done) => {
-      const today = moment();
-      const yesterday = today.subtract(1, "days");
-      const lastMonth = today.subtract(1, "month");
-      const format = (m) => m.format("YYYY.MM.DD");
-
       const expected = {indices: {}};
       expected.indices[`logs-${format(today)}`] = [];
       expected.indices[`logs-${format(yesterday)}`] = [];
@@ -42,9 +39,38 @@ describe("elasticsearch", () => {
         assert.deepEqual(indices, [`logs-${format(lastMonth)}`]);
         fakeES.done();
         done();
-      }).catch((err) => {
-        done(err);
-      });
+      }).catch(done);
+    });
+  });
+
+  describe("update_aliases", () => {
+    it("should make one command to put aliases to the right state", (done) => {
+      const current = {};
+      current[`logs-${format(yesterday)}`] = {aliases: {last_2days: {}}};
+      current[`logs-${format(lastMonth)}`] = {aliases: {last_2days: {}}};
+
+      const expected = {};
+      expected[`logs-${format(yesterday)}`] = {aliases: {last_2days: {}}};
+      expected[`logs-${format(today)}`] = {aliases: {last_2days: {}}};
+
+      const fakeES = nock(process.env.ELASTICSEARCH_URL)
+        .get("/_aliases")
+        .reply(200, current)
+        .post("/_aliases",
+              {actions: [
+                {remove: {index: `logs-${format(lastMonth)}`, alias: "last_2days"}},
+                {add: {index: `logs-${format(today)}`, alias: "last_2days"}},
+              ],
+              })
+        .reply(200)
+        .get("/_aliases")
+        .reply(200, expected);
+      es.update_aliases().then((aliases) => {
+        assert.deepEqual(aliases,
+                         {last_2days: [`logs-${format(yesterday)}`, `logs-${format(today)}`]});
+        fakeES.done();
+        done();
+      }).catch(done);
     });
   });
 });
